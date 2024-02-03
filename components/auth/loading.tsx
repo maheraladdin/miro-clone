8065a29093c433ced71a84d5ee5873ee77e20622
maheraladdin <73165350+maheraladdin@users.noreply.github.com>
2024-02-03 23:34:23 +0200

    Setup Organization Sidebar & navbar.tsx

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -4,11 +4,11 @@ export default function Loading() {
   return (
     <div className={"flex h-screen w-screen items-center justify-center"}>
       <Image
-        src="/miro.png"
+        src="/miro.svg"
         alt="miro logo"
         width={120}
         height={120}
-        className={"animate-pulse duration-700 ease-in-out"}
+        className={"animate-pulse rounded-md duration-700 ease-in-out"}
         sizes={"120px"}
       />
     </div>
